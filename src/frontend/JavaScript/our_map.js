6b6af8c3affd0bf87c17c0eb9249ea1d82175111
89605502155 <ferubko1999@yandex.ru>
2022-03-21 19:15:43 +0300

    Map is working

--- src/frontend/JavaScript/our_map.js
+++ src/frontend/JavaScript/our_map.js
@@ -1,14 +1,17 @@
-var map = new ol.Map({
-target: 'map',
-layers: [
-  new ol.layer.Tile({
-    source: new ol.source.OSM()
-  })
-],
-view: new ol.View({
-  center: ol.proj.fromLonLat([0, 0]),
-  zoom: 3
-})
-]
-});
+window.onload=init
+
+function init(){
+	const map=new ol.Map({
+		view: new ol.View({
+			center: [0,0],
+			zoom: 2
+		}),
+		layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM()
+			})
+		],
+		target: "map"
+	})
+}
 
