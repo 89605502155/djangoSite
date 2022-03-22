f11bd73f2535b4a87b13dc9d956b2033ca938aca
89605502155 <ferubko1999@yandex.ru>
2022-03-22 03:40:50 +0300

    ogc_map way not working now

--- src/frontend/JavaScript/our_map.js
+++ src/frontend/JavaScript/our_map.js
@@ -1,21 +1,22 @@
+import 'ol/ol.css';
+
 window.onload=init
 
 function init(){
 	const map=new ol.Map({
 		view: new ol.View({
+			projection: 'EPSG:4326',
 			center: [12333691.37451099, 14420057.976483416],
 			zoom: 2
 		}),
 		layers: [
-			new ol.layer.Tile({
-				source: new ol.source.OSM()
-			})
+			new ol.TileLayer({
+			  source: new ol.OGCMapTile({
+				url: 'https://maps.ecere.com/ogcapi/collections/blueMarble/map/tiles/WorldCRS84Quad',
+			  }),
+			}),
 		],
 		target: "js-map"
-	})
-
-	map.on('click', function(e){
-		console.log(e.coordinate);
 	})
 }
 
