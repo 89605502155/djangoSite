4d380af7807fa60a23ea4805bc4641a0ba011102
89605502155 <ferubko1999@yandex.ru>
2022-03-23 01:34:58 +0300

    make some refactoring

--- src/frontend/JavaScript/our_map.js
+++ src/frontend/JavaScript/our_map.js
@@ -1,3 +1,7 @@
-
+var map = L.map('map').setView([28.3949, 84.1240], 8);
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+osm.addTo(map);
 
 
