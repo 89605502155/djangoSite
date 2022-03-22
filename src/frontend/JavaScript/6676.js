7c05f34370db26d2d8a968cd9b14b92fdae1764a
89605502155 <ferubko1999@yandex.ru>
2022-03-23 00:39:04 +0300

    I cleaned some bags after merged.

--- src/frontend/JavaScript/6676.js
+++ src/frontend/JavaScript/6676.js
@@ -46,3 +46,33 @@ for (var i = 0; i < data.length; ++i) {
 }
 
 map.setView([data[0][1], data[0][2]], 10);
+
+12333691.37451099, 14420057.976483416
+
+
+window.onload=init
+
+function init(){
+	const map=new ol.Map({
+		view: new ol.View({
+			center: [12333691.37451099, 14420057.976483416],
+			zoom: 2
+		}),
+		layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM()
+			})
+		],
+		target: "js-map"
+	})
+
+	map.on('click', function(e){
+		console.log(e.coordinate);
+	})
+	
+	var marker = new Feature(
+		new MultiPoint([
+			[80,99]
+			[66,1], opt_layout)
+		);
+}
