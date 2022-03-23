bf8e0fcafcffd64f4a299e0d1edc771823259126
89605502155 <ferubko1999@yandex.ru>
2022-03-23 19:25:12 +0300

    Now the header has the fixced position and zindex

--- src/frontend/JavaScript/our_map.js
+++ src/frontend/JavaScript/our_map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([75, 84.1240], 3.1);
+var map = L.map('map').setView([75, 124.1240], 3.5);
 /*
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
