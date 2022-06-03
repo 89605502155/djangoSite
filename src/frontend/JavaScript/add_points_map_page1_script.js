bebb9c3ddfdd0edde845ed6627f968c33bb0c66d
89605502155 <ferubko1999@yandex.ru>
2022-06-03 23:43:20 +0300

    we go to the output the form of data.

--- src/frontend/JavaScript/add_points_map_page1_script.js
+++ src/frontend/JavaScript/add_points_map_page1_script.js
@@ -1,13 +1,35 @@
 const addButton = document.getElementById("addPointsButtonid");
 
-const firstUser={
-    username: "admin",
-    password: "qwerty"
-}
+const users=[
+    {
+        username: "admin",
+        password: "qwerty"
+    },
+    {
+        username: "scientist",
+        password: "1234"
+    }
+];
+
+var continueAdded=false;
 addButton.addEventListener("click",function(){
     var username=prompt("Give me your username");
-    username===firstUser.username ? console.log("Good usn"):console.log("Bad very bad");
-    console.log(firstUser.username);
+    let userid=-1;
+    for(var i=0;i<users.length;i++){
+        if(username===users[i].username){
+            userid=i;
+            break;
+        }
+    }
+    if(userid===-1){
+        alert("Your user name is not good, try again.");
+        return 0;
+    }
+    // username===firstUser.username ? console.log("Good usn"):console.log("Bad very bad");
+    console.log(users[userid].username);
     var password=prompt("Your password");
-    password===firstUser.password? console.log("Good password"):console.log("Bad password");
+    password===users[userid].password? continueAdded=true : alert("Your password or username is bad");
 });
+// if(continueAdded){
+
+// }
