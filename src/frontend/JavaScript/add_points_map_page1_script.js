9f7513ffffb21f8e77d709a5d71d7fe862b31e77
89605502155 <ferubko1999@yandex.ru>
2022-06-06 18:41:46 +0300

    some proposed to work with id

--- src/frontend/JavaScript/add_points_map_page1_script.js
+++ src/frontend/JavaScript/add_points_map_page1_script.js
@@ -13,45 +13,38 @@ const addButton = document.getElementById("addPointsButtonid");
 
 
 addButton.addEventListener("click",function(){
-    addButton.remove();
-    var txt1 = '<form action="src/backend/afrorization_1.php" method="POST">'+
-    '<div class="mb-3">'+
-    '<label for="inputUserName1" class="form-label">User name</label>'+
-    '<input type="text" class="form-control" id="inputUserName1" name="inputUserName1">'+
-    '</div>'+
-    '<div class="mb-3">'+
-    '<label for="inputPassword1" class="form-label">Password</label>'+
-    '<input type="text" class="form-control" id="inputPassword1" name="inputPassword1">'+
-    '</div>'+
-    '<button type="submit" class="btn btn-primary" id="mapPagesubmitButton1">Submit</button>'+
-    '</form>'+
-    '<script>'+
-    '$(document).ready(function(){'+
-    '    $(\'form\').submit(function(event){'+
-    '        event.preventDefault();'+
-    '        if ($(\'#inputUserName1\').val()==="" || $(\'#inputPassword1\').val()===""){'+
-    '            alert("You want to sent a pass string");'+
-    '        }'+
-    '        console.log($(this).serialize());'+
-    '        $.ajax({'+
-    '            type: "POST",'+
-    '            url: "src/backend/afrorization_1.php",'+
-    '            data: $(this).serialize(),'+
-    '            success: function(response)'+
-    '            {'+
-    '                var jsonData = response;'+
-    '                if (jsonData.success == "1")'+
-    '                {'+
-    '                    alert("Good");'+
-    '                }'+
-    '                else'+
-    '                {'+
-    '                    alert("Invalid Credentials!");'+
-    '                }'+
-    '             }'+
-    '        })'+
-    '    });'+
-    '}); '+
+    addButton.style.display="none";
+    // addButton.remove();
+    const form1=document.getElementById("authenticForm");
+    form1.style.display="block";
+    var txt1 = 
+    '<script src="src/frontend/JavaScript/add_points_map_page2_script.js">'+
+    // '$(document).ready(function(){'+
+    // '    $(\'form\').submit(function(event){'+
+    // '        event.preventDefault();'+
+    // '        if ($(\'#inputUserName1\').val()==="" || $(\'#inputPassword1\').val()===""){'+
+    // '            alert("You want to sent a pass string");'+
+    // '        }'+
+    // '        console.log($(this).serialize());'+
+    // '        $.ajax({'+
+    // '            type: "POST",'+
+    // '            url: "src/backend/afrorization_1.php",'+
+    // '            data: $(this).serialize(),'+
+    // '            success: function(response)'+
+    // '            {'+
+    // '                var jsonData = response;'+
+    // '                if (jsonData.success == "1")'+
+    // '                {'+
+    // '                    alert("Good");'+
+    // '                }'+
+    // '                else'+
+    // '                {'+
+    // '                    alert("Invalid Credentials!");'+
+    // '                }'+
+    // '             }'+
+    // '        })'+
+    // '    });'+
+    // '}); '+
     '</script>';
 
     $("main").append(txt1);
--- src/frontend/JavaScript/add_points_map_page2_script.js
+++ src/frontend/JavaScript/add_points_map_page2_script.js
@@ -1,2 +1,35 @@
-// const submitButton1=document.getElementById('mapPagesubmitButton1');
+$(document).ready(function(){
+    $('form').submit(function(event){
+        event.preventDefault();
+        if ($('#inputUserName1').val()==="" || $('#inputPassword1').val()===""){
+            alert("You want to sent a pass string");
+        }
+        // console.log($(this).serialize());
+        $.ajax({
+            type: "POST",
+            url: "src/backend/afrorization_1.php",
+            data: $(this).serialize(),
+            success: function(jsonData)
+            {
+                // var jsonData = response;
+                if (jsonData.success == "1")
+                {
+                    // var txt2='<script src="src/frontend/JavaScript/add_points_map_page3_script.js"></script>';
+                    // $("main").append(txt2);
+                    alert("Great!");
+                }
+                else
+                {
+                    const formForRemove=document.getElementById("authenticForm");
+                    formForRemove.style.display="none";
+                    alert("Invalid Credentials!");
+                    const addButton = document.getElementById("addPointsButtonid");
+                    addButton.style.display="block";
+                    // var txt2='<script src="src/frontend/JavaScript/add_points_map_page1_script.js"></script>';
+                    // $("main").append(txt2);
+                }
+            }
+        })
+    });
+});
 
