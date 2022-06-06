372c94bb1ed0a08b0be6bab879d90b7aab3b9729
89605502155 <ferubko1999@yandex.ru>
2022-06-06 04:07:59 +0300

    I go to aftorization with json.

--- src/backend/afrorization_1.php
+++ src/backend/afrorization_1.php
@@ -1,5 +1,47 @@
 <?php
 	$name=$_POST['inputUserName1'];
 	$password=$_POST['inputPassword1'];
-	echo "user name: ".$name."<br>"."Password: ".$password;
-?>
+	echo "user name: ".$name."<br>"."Password: ".$password."<br>";
+	// $fd = fopen("jsonFolder/users.json", 'r') or die("не удалось открыть файл");
+	// while(!feof($fd))
+	// {
+	//     $str = htmlentities(fgets($fd));
+	//     echo $str."<br>string<br>";
+	// }
+	// fclose($fd);
+
+
+	// $strJsonFileContents = file_get_contents("jsonFolder/users.json");
+	// $array = json_decode($strJsonFileContents, true);
+	// var_dump($array);
+
+
+	// $people_json = file_get_contents('jsonFolder/users.json');
+
+	// $decoded_json = json_decode($people_json, false);
+	// echo $decoded_json;
+
+
+	$filename = 'jsonFolder/users.json';
+
+	$data = file_get_contents($filename);
+	$users = json_decode($data);
+	echo $users[0]->username;
+	?>
+
+	<html>
+	<table>
+	    <tbody>
+	        <tr>
+	            <th>Name</th>
+	            <th>Password</th>
+	        </tr>
+	        <?php foreach ($users as $user) { ?>
+	        <tr>
+	            <td> <?= $user->username; ?> </td>
+	            <td> <?= $user->password; ?> </td>
+	        </tr>
+	        <?php } ?>
+	    </tbody>
+	</table>
+	</html>
