a34eea2d53e5bb5f1a37876114daf0b17abfeca0
89605502155 <ferubko1999@yandex.ru>
2022-06-08 04:12:36 +0300

    We append new points in new json.

--- src/backend/add_poins_1.php
+++ src/backend/add_poins_1.php
@@ -1,3 +1,53 @@
 <?php 
-echo "Your letter is come";
+    $name=$_POST['inputNameAddedStation1'];
+    $longitude=$_POST['inputLongitudeAddedStation1'];
+    $latitude=$_POST['inputLatitudeAddedStation1'];
+    $numberDepths=$_POST['inputDepthAddedStation1'];
+
+    $filename = 'jsonFolder/added_points.json';
+
+    
+    $data = file_get_contents($filename);
+    flock($filename, LOCK_EX);
+	$stations = json_decode($data);
+    for($i=0;$i<sizeof($stations);$i++){
+        if($stations[$i]->name == $name){
+            echo json_encode(array('success' => 0));
+            return 0;
+        }
+    }
+    $numDep = [];
+    for ($i=0;$i<(int)$numberDepths;$i++){
+        $dep=$_POST['addedPointDepth'.(string)$i];
+        $sal=$_POST['addedPointSalinit'.(string)$i];
+        $doc=$_POST['addedPointDoc'.(string)$i];
+        array_push($numDep,(object)[
+            'depth'=>$dep,
+            'salinity'=>$sal,
+            'DOC'=>$doc
+        ]);
+    }
+    $obj = (object)[
+        'name'=>$name,
+        'lat'=>$latitude,
+        'lon'=>$longitude,
+        'depths'=>$numDep
+    ];
+    
+    array_push($stations,$obj);
+    file_put_contents('jsonFolder/added_points.json',json_encode($stations));
+    flock($filename, LOCK_UN);
+    echo json_encode(array('success' => 1));
+    // $name="hh";
+    // $sur="878787";
+    // $obj = (object) [
+    //     'hj'=>[
+    //     'aString' => $name,
+    //     'anArray' => $sur
+    //     ]
+    // ];
+    
+    // echo json_encode($obj);
+
+
 ?>
